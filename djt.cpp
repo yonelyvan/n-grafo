6151234e18f1e83569a2dd520394a2f524afd89c
levesques <yonelyvan@gmail.com>
2013-11-09 00:54:08 -0300

    Update djt.cpp
    implementacion dijkstra

--- djt.cpp
+++ djt.cpp
@@ -1,81 +1,96 @@
 #include <iostream> 
-#define inf 100 
-using namespace std; 
-                   //1   2   3   4   5    6
-int matrix[6][6] = {{0,  7,  3,  inf,inf,inf},  //1
-					{7,  0,  1,  6,  inf,inf},  //2
-					{3,  1,  0,  3,  8,  inf},  //3
-					{inf,6,  3,  0,  2,  2  },  //4
-					{inf,inf,8,  2,  0,  8  },  //5
-					{inf,inf,inf,2,  8,  0  }}; //6
+#include <vector>
+#define inf 100  // valor infinito'
+using namespace std;
 
+ 
+int M[6][6]={{0,  7,  3,  inf,inf,inf}, 
+		     {7,  0,  1,  6,  inf,inf}, 
+		     {3,  1,  0,  3,  8,  inf}, 
+		     {inf,6,  3,  0,  2,  2  },  
+	         {inf,inf,8,  2,  0,  8  },  
+		     {inf,inf,inf,2,  8,  0  }}; 	
+ 
+ 
 int min(int a,int b){if(a<b){return a;} else return b;}
 
-void copiar(int M[6][6],int size_nodos,int vec[6]){
-	for(int i=0;i<size_nodos;i++){
-		vec[i]=M[0][i];
-		}		
-	for(int j=0;j<size_nodos;j++)
-	{cout<<"pos "<<j<<" : "<<vec[j]<<endl;}
-    
-	}
 
-bool hay_cero(int vec_bool[6],int size_nodos){
-	for(int i=0;i<size_nodos;i++){
+
+template<class T>
+void copiar(int **matrix,vector<T> &d){
+	for(int i=0;i<d.size();i++){
+		d[i]=matrix[0][i];
+		}		
+}
+
+
+
+
+template<class T>
+bool hay_cero(vector<T> &vec_bool){
+	for(int i=0;i<vec_bool.size();i++){
 		if(vec_bool[i]==0){return true;}
 		}
 	return false;
 	}
 
-int get_min(int vec_bool[],int d[],int size_nodos){
-	int indice_min;
-	int j;
-	for(int i=0;i<size_nodos;i++){
-		if(vec_bool[i]==0){
-			j=i;
-			indice_min = i; 
-			break;}
-      }
-    j++;
-     for(j;j<6;j++){
-		 if(vec_bool[j]==0){
-			 if(d[j] < d[indice_min]){
-				 indice_min=j;
+
+
+template<class B,class T>
+int get_min(vector<B> &vec_bool,vector<T> &d){
+	int indice_min=0;
+	int j=0;
+	while(!indice_min){
+     		if(vec_bool[j]==0){
+	    		indice_min = j;
+			   }
+		    j++;
+	}
+	for(j;j<d.size();j++){
+		if( vec_bool[j]==0 && d[j] < d[indice_min]){
+			indice_min =j;
 			}
-		}	 
-     } 
+	}
+ 
      return indice_min;
 }
 
-void djt(){
+
+//--------------------------------------------------------
+
+template<class T>
+vector<int> djt(T **matrix,int size_matrix){
 	//creamos los vectores 
-	int c[6]={0,0,0,0,0,0}; //llenamos con ceros 
-	int d[6];
-	
-	c[0]=1;//inicialisamos 
-	copiar(matrix,6,d);//mejorar para 6 (codigo duro)
-	//algoritmo boras O.o
-	while(hay_cero(c,6)){
-		int p = get_min(c,d,6);
-		c[p]=1;//seccionado / marcamos 1  en el vector de bools
-	
-		for(int i=0;i<6;i++){
+	vector<bool> c(size_matrix); for(int i=0;i<c.size();i++){c[i]=0;}//llenamos con ceros 
+	vector<int> d(6);
+	c[0]=1;//inicialisamos con un nodo 
+	copiar(matrix,d);
+
+	while(hay_cero(c)){
+		int p = get_min(c,d);
+		c[p]=1;//seccionado / marcamos 1  en el vector booleano 
+		for(int i=0;i<d.size();i++){
 			if(c[i]==0){
 				d[i] = min(d[i],(d[p]+matrix[p][i]));
 			}
 	     }	
 	}
-	for(int j=0;j<6;j++){
-		cout<<d[j]<<" ";
-		}
-		cout<<endl;
+	return d;
 }
 
 
 
 
+
 int main(){
-	djt();
-	
+	int **matriz;
+matriz=new int*[6];
+for(int i=0; i<6; i++){matriz[i]=new int[6];} //crear
+for(int i=0; i<6; i++){for(int j=0; j<6; j++){matriz[i][j]=M[i][j];}} //lenar
+
+vector<int> n(6);
+n=djt(matriz,6);
+
+for(int i=0;i<n.size();i++){cout<<n[i]<<" ";}cout<<endl;
 	return 0;
 	}
